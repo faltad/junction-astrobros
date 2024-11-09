ab2179d198d44d29c17b90ef2413d72c826d8318
Horia Gug <horia.gug@yousician.com>
2024-11-09 12:07:21 +0200

    daterange api

--- packages/models.py
+++ packages/models.py
@@ -1,7 +1,14 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 
 @dataclass
 class Coords:
     latitude: float
     longitude: float
+
+
+@dataclass
+class DateRange:
+    start_date: datetime
+    end_date: datetime
