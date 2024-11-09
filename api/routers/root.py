24e179a3c902034b2d0582c66f2a23d44030857d
Horia Gug <horia.gug@yousician.com>
2024-11-09 11:24:49 +0200

    updates after merge

--- api/routers/root.py
+++ api/routers/root.py
@@ -5,7 +5,6 @@ from api.dependencies import get_settings
 from packages.models import Coords
 from packages.sentinel import get_true_colors_sentinel2
 from fastapi.responses import StreamingResponse
-import io
 import config
 
 router = APIRouter()
@@ -23,15 +22,11 @@ def get_image(
     settings: Annotated[config.Settings, Depends(get_settings)], lat: float, long: float
 ):
     file_content = get_true_colors_sentinel2(
-        coords=Coords(latitude=lat, longitude=long)
+        config=settings.prepare_sh_config(), coords=Coords(latitude=lat, longitude=long)
     )
-
-    if file_content is None:
-        file_content = "Just for testing"
-    file_like_object = io.BytesIO(file_content.encode("utf-8"))
     # media_type here sets the media type of the actual response sent to the client.
     return StreamingResponse(
-        file_like_object,
+        file_content,
         media_type="text/plain",
         headers={"Content-Disposition": "attachment; filename=example.txt"},
     )
